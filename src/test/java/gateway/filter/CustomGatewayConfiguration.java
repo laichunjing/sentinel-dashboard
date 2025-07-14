70a2aa92c2f5cdf2f5b91d25aa480e6b3909dac6
laichunjing <15279055894@163.com>
2025-07-14 11:20:40 +0800

    补充gateway限流的描述和API分组的限流说明

--- src/test/java/gateway/filter/CustomGatewayConfiguration.java
+++ src/test/java/gateway/filter/CustomGatewayConfiguration.java
@@ -1,19 +1,25 @@
 /*
 package com.ugreen.cloud.gateway.config;
 
+import com.alibaba.cloud.sentinel.gateway.scg.SentinelGatewayProperties;
 import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
 import com.ugreen.cloud.gateway.filter.RealIpRequestItemParser;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @Slf4j
 public class CustomGatewayConfiguration {
+    @Autowired
+    private SentinelGatewayProperties gatewayProperties;
 
     @Bean
+    @Primary
     @ConditionalOnProperty(
             prefix = "spring.cloud.sentinel.scg.filter",
             name = {"enabled"},
@@ -24,7 +30,7 @@ public class CustomGatewayConfiguration {
         // 返回 null 或不创建 SentinelGatewayFilter 的实例
         // return null;
         log.info("Initializing custom Sentinel filter with real IP support");
-        return new SentinelGatewayFilter(new RealIpRequestItemParser());
+        return new SentinelGatewayFilter(gatewayProperties.getOrder(),new RealIpRequestItemParser());
     }
 }
 */
